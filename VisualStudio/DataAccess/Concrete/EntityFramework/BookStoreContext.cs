c4f2fcea737a7b7a8f47fb6f91de03821a5dcd88
Batuhan BAŞTÜRK <37836695+bthnbstrk@users.noreply.github.com>
2020-04-30 05:50:58 +0300

    Commit

---
+++ VisualStudio/DataAccess/Concrete/EntityFramework/BookStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class BookStoreContext:DbContext
+    {
+        //DB tablosu ile entity eşleştirme
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BookStore;Trusted_Connection=true");
+        }
+
+        public DbSet<Book> Books { get; set; } 
+    }
+}
---
+++ VisualStudio/DataAccess/Concrete/EntityFramework/EfBookDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+using Entities.Concrete;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfBookDal:EfEntityRepositoryBase<Book,BookStoreContext>,IBookDal
+    {
+    }
+}
